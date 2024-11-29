75716ed28fe5ec619bf2a1ac6f2c1b3ee204320e
Evelyn R <evelynrodriguezc0@gmail.com>
2024-11-29 00:06:59 -0500

    adding date library

--- hello_app.rb
+++ hello_app.rb
@@ -2,7 +2,5 @@ require 'sinatra'
 require './day'
 
 get '/' do
-#    dayname = Date::DAYNAMES[Time.now.wday]
-#    "Hello, world! Happy #{dayname}"
     "Hello, world! Happy #{day_of_the_week(Time.now)}"
 end

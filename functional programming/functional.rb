e0c72ebee0e319956f42ab06c87bb27608fa4d0d
Evelyn R <evelynrodriguezc0@gmail.com>
2024-12-01 19:10:40 -0500

    map funct

---
+++ functional programming/functional.rb
@@ -0,0 +1,26 @@
+states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
+
+# Returns a URL-friendly version of a string.
+# Example: "North Dakota" -> "north-dakota"
+def urlify(string)
+    string.downcase.split.join("-")
+end
+
+#urls: Imperative version
+def imperative_urls(states)
+    urls = []
+    states.each do |state|
+        urls << urlify(state)
+    end
+    urls
+end
+
+p imperative_urls(states)
+# puts imperative_urls(states).inspect
+
+#urls: Functional version
+def functional_urls(states)
+    states.map { |state| urlify(state) }
+end
+
+p functional_urls(states)

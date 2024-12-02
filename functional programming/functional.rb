c3e61b9c22dc9dbf0bbd641b1177d0555d7fc4c9
Evelyn R <evelynrodriguezc0@gmail.com>
2024-12-01 23:43:02 -0500

    adding more function states

--- functional programming/functional.rb
+++ functional programming/functional.rb
@@ -24,3 +24,45 @@ def functional_urls(states)
 end
 
 p functional_urls(states)
+
+# Returns a full URL for each state.
+def full_urls(states)
+    states.map { |state| "https://example.com/#{urlify(state)}" }
+end
+
+p full_urls(states)
+
+
+# singles: Imperative version
+def imperative_singles(states)
+    singles = []
+    states.each do |state|
+        if state.split.length == 1
+            singles << state
+        end
+    end
+    singles
+end
+
+p imperative_singles(states)
+
+
+# singles: Functional version
+def functional_singles(states)
+    states.select { |state| state.split.length == 1 }
+end
+
+p functional_singles(states)
+
+
+def dakota_states(states)
+    states.select { |state| state.include?("Dakota") }
+end
+
+p dakota_states(states)
+
+def double_states(states)
+    states.select { |state| state.split.length == 2 }
+end
+
+p double_states(states)
